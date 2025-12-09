12c76f6f5d2a6757c6962216d9fb37102a9779e6
Verqari <peter.zimny@outlook.com>
2025-12-09 18:54:06 +0000

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 // ⚠️ REPLACE THESE WITH YOUR REAL VALUES
-const BITGET_API_KEY = process.env.BITGET_API_KEY || "bg_614d0aff7aed6eb327bd7256d34ba676";
-const BITGET_API_SECRET = process.env.BITGET_API_SECRET || "14040178";
-const BITGET_API_PASS = process.env.BITGET_API_PASS || "973114d0727fe00f0df88baae06c6a36d43cb3150d989114bc03782a840627fa";
+const API_KEY = "bg_614d0aff7aed6eb327bd7256d34ba676";
+const API_SECRET = "14040178";
+const PASSPHRASE = "973114d0727fe00f0df88baae06c6a36d43cb3150d989114bc03782a840627fa";
 const BRIDGE_SECRET = "eyJhbGciOiJIUzI1NiJ9.eyJzaWduYWxzX3NvdXJjZV9pZCI6MTU1Mjc1fQ.9Tph5w-fPgUVMS7hCPkqe5RBMsmBAUsTxC8BWTuTL9E";
 
 // =====================================
